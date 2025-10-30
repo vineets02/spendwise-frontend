99631970c1caf4ac51a7bc84579b88fd3651da49
vineets02 <vineet.shrivastava710@gmail.com>
2025-10-30 09:52:24 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,18 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()], server: { port: 5173, host: true } })
+// export default defineConfig({ plugins: [react()], server: { port: 5173, host: true } })
+export default defineConfig({
+  plugins: [react()],
+  // if you deploy under a subpath (GH Pages), set base. For Render root, keep "/".
+  base: '/',
+  server: {
+    host: true,
+    // allow Render domain (change to your actual onrender hostname)
+    allowedHosts: ['spendwise-frontend-wwqe.onrender.com'],
+  },
+  preview: {
+    host: true,
+    allowedHosts: ['spendwise-frontend-wwqe.onrender.com'],
+  },
+})
